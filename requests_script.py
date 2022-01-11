b71cd2212c4f5c48ab60f3de8899c40145360f25
osmani2 <natasha.osmani@gmail.com>
2022-01-10 19:25:48 -0500

    GET google homepage

--- requests_script.py
+++ requests_script.py
@@ -1,3 +1,12 @@
 import requests as rs
 
+'''
+Gets the version of the requests library
+'''
 print(rs.__version__)
+
+'''
+GET the google homepage
+'''
+res = rs.get("https://www.google.com/")
+print("STATUS CODE: ",res.status_code,"\n","CONTENT: ",res.content)
